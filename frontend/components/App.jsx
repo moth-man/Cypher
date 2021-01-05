9b1f1e861358c054b607e9175f14df7f1af36f77
Moth-Man <moth-man@Moth-Mans-MacBook-Pro.local>
2021-01-05 00:21:21 -0500

    Integrate material-ui layout for landing page

---
+++ frontend/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Landing } from './layouts';
+
+export default function App() {
+  return (
+    <Landing>
+      <div>
+        <h1>Cypher</h1>
+      </div>
+    </Landing>
+  );
+}
