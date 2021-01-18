6fd63fbcbd6cb85f774be3573f299ca0abb963e5
Moth-Man <moth-man@Moth-Mans-MacBook-Pro.local>
2021-01-17 20:27:04 -0700

    Receive and send calls to other users connected in web-socket rooms.

--- frontend/components/Cypher.jsx
+++ frontend/components/Cypher.jsx
@@ -4,52 +4,58 @@ import { Switch, Route } from 'react-router-dom';
 import Peer from 'peerjs';
 import io from 'socket.io-client';
 import Stream from './Stream';
-import { SocketContext } from './contexts';
 
 export default function Cypher() {
+  const [myStream, setMyStream] = useState(null);
   const [streams, setStreams] = useState([]);
 
   useEffect(() => {
     (async function getStream() {
-      setStreams([await navigator.mediaDevices.getUserMedia({
+      setMyStream(await navigator.mediaDevices.getUserMedia({
         video: true,
         audio: false,
-      })]);
+      }));
     }());
   }, []);
 
-  if (!streams.length) {
+  useEffect(() => {
+    if (!myStream) return;
+
+    const socket = io('http://localhost:3000');
+
+    const peer = new Peer(undefined, {
+      path: '/peerjs',
+      host: '/',
+      port: '3000',
+    });
+
+    // Them calling us.
+    peer.on('call', (call) => {
+      call.answer(myStream);
+      call.on('stream', (stream) => {
+        console.log('call from', stream);
+      });
+    });
+
+    peer.on('open', (id) => {
+      socket.emit('join-room', 'User connected.', id);
+    });
+
+    // Us calling them.
+    socket.on('user-connected', (userID) => {
+      const call = peer.call(userID, myStream);
+      call.on('stream', (stream) => {
+        console.log('call to', stream);
+      });
+    });
+  }, [myStream]);
+
+  if (!myStream) {
     return null;
   }
 
-  const socket = io('http://localhost:3000');
-  const peer = new Peer(undefined, {
-    path: '/peerjs',
-    host: '/',
-    port: '3000',
-  });
-
-  // Them calling us.
-  peer.on('call', (call) => {
-    call.answer(streams[0]);
-    call.on('stream', (stream) => {
-      setStreams(streams.concat(stream));
-    });
-  });
-
-  peer.on('open', (id) => {
-    socket.emit('join-room', 'User connected.', id);
-  });
-
-  // Us calling them.
-  socket.on('user-connected', (userID) => {
-    const call = peer.call(userID, streams[0]);
-    call.on('stream', (stream) => {
-      setStreams(streams.concat(stream));
-    });
-  });
-
+  console.log('my stream', myStream);
   return (
-    streams.map((stream) => <Stream stream={stream} />)
+    [myStream].concat(streams).map((stream) => <Stream stream={stream} />)
   );
 }
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import io from 'socket.io-client';
 import App from './App';
-import { SocketContext } from './components/contexts';
-
-const socket = io('http://localhost:3000');
 
 ReactDOM.render(
   <BrowserRouter>
-    <SocketContext.Provider value={socket}>
-      <App />
-    </SocketContext.Provider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
